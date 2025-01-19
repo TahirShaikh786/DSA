eaaeea41fa8877b8e96636f56d0b82cd24ac890f
TahirShaikh786 <altahirshaikh786.com>
2025-01-19 22:00:48 +0530

    Completed 2 Question of BS

--- 07_Binary_Search/Question_01_Ceiling.java
+++ 07_Binary_Search/Question_01_Ceiling.java
@@ -1,7 +1,7 @@
 public class Question_01_Ceiling {
   public static void main(String[] args) {
     int[] ascNums = {2, 3, 5, 9, 14, 16, 18};
-    int ascTar = 10;
+    int ascTar = 20;
     int ans = Ceiling_Of_number(ascNums, ascTar);
     System.out.println("The Ascending target is " + ascTar + " & Ceiling Number is " + ascNums[ans]);
 
@@ -11,10 +11,19 @@ public class Question_01_Ceiling {
     System.out.println("The Descending target is " + descTar + " & Ceiling Number is " + descNums[ans2]);
   }
 
+  /*
+   * Return the index of
+   * the Smallest no >= target
+   */
   static int Ceiling_Of_number(int[] arr, int target){
     int start = 0;
     int end = arr.length - 1;
     boolean isAsc = arr[start] < arr[end];
+
+    // But what if the target is greater than the greatest number in the array
+    if(target > arr[end]){
+      return -1;
+    }
 
     while(start <= end){
       int mid = start + (end - start) / 2;
--- 07_Binary_Search/Question_02_Floor.java
+++ 07_Binary_Search/Question_02_Floor.java
@@ -1,33 +1,42 @@
 public class Question_02_Floor {
   public static void main(String[] args) {
-    int[] ascNums = {2, 3, 5, 9, 14, 16, 18};
+    int[] ascNums = { 2, 3, 5, 9, 14, 16, 18 };
     int ascTar = 14;
     int ans = FloorOfNumber(ascNums, ascTar);
     System.out.println(ans);
     System.out.println("The Ascending Array target Number is " + ascTar + " & the Floor Value is " + ascNums[ans]);
   }
 
-  static int FloorOfNumber(int[] arr, int target){
+  /*
+   * return the index of
+   * the greatest no <= target
+   */
+  static int FloorOfNumber(int[] arr, int target) {
     int start = 0;
     int end = arr.length - 1;
-    
+
     boolean isAsc = arr[start] < arr[end];
-    
-    while(start <= end){
+
+    // But what if the target is greater than the greatest number in the array
+    if (target > arr[end]) {
+      return -1;
+    }
+
+    while (start <= end) {
       int mid = start + (end - start) / 2;
-      if(target == arr[mid]){
+      if (target == arr[mid]) {
         return mid;
       }
-      if(isAsc){
-        if(target < arr[mid]){
+      if (isAsc) {
+        if (target < arr[mid]) {
           end = mid - 1;
-        }else{
+        } else {
           start = mid + 1;
         }
-      }else{
-        if(target < arr[mid]){
+      } else {
+        if (target < arr[mid]) {
           start = mid + 1;
-        }else{
+        } else {
           end = mid - 1;
         }
       }
