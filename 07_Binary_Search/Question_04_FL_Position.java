7514c60a50fc1c034fc7072db99cb2035feb348b
TahirShaikh786 <altahirshaikh786.com>
2025-01-21 23:45:20 +0530

    Completed First and Last Occurrence Position Of Number in Sorted Array

--- 07_Binary_Search/Question_04_FL_Position.java
+++ 07_Binary_Search/Question_04_FL_Position.java
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+
 import java.util.Arrays;
 
 public class Question_04_FL_Position {
@@ -8,6 +10,39 @@ public class Question_04_FL_Position {
   }
 
   static int[] searchRange(int[] nums, int target) {
+    int[] ans = { -1, -1 };
+
+    // Check for First Occurence if target found
+    int start = searchOccur(nums, target, true);
+    int end = searchOccur(nums, target, false);
+    ans[0] = start;
+    ans[1] = end;
+
+    return ans;
+  }
+
+  // this function just return the index value of target
+  static int searchOccur(int[] nums, int target, boolean findStartIndex){
+    int ans = -1;
+    int start = 0;
+    int end = nums.length - 1;
     
+    while(start <= end){
+      int mid = start + (end - start) / 2;
+      if(target < nums[mid]){
+        end = mid - 1;
+      }else if(target > nums[mid]){
+        start = mid + 1;
+      }else{
+        // Potential ans found
+        ans = mid;
+        if(findStartIndex){
+          end = mid - 1;
+        }else{
+          start = mid + 1;
+        }
+      }
+    }
+    return ans;
   }
 }
